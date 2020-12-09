386711d1e803f737e00576767212c5133129500d
Chris Ramer <chrisramer.97@gmail.com>
2020-12-09 11:51:35 -0800

    Run API request, does not output response yet

--- src/main.js
+++ src/main.js
@@ -2,10 +2,30 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// Examples of imports
-//import Triangle from './js/triangle.js';
-//import Rectangle from './js/rectangle.js';
+
+async function ConvertCurrency(currency) {
+	new Promise(function(resolve, reject) {
+		let request = new XMLHttpRequest();
+		const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`;
+		request.onload = function() {
+			if (this.status === 200) {
+				resolve(request.response);
+				console.log("SUCCESS!");
+			} else {
+				reject(request.response);
+				console.log("FAILURE :(");
+			}
+		}
+		request.open("GET", url,  true);
+		request.send();
+	})
+}
 
 $(document).ready(function () {
-
+	$("form#currencyForm").submit(function (e) { 
+		e.preventDefault();
+		const input = $("#currency").val();
+		const currency = input.substring(input.length - 4, input.length - 1);
+		ConvertCurrency(currency)
+	});
 });
